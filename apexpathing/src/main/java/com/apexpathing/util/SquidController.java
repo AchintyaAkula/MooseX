ed6fbb1ef98632b3117d5efcc4c5c584700fcbf1
Xhaemel <xhaemel@gmail.com>
2026-03-21 22:27:10 -0700

    inital commit class,

---
+++ apexpathing/src/main/java/com/apexpathing/util/SquidController.java
@@ -0,0 +1,10 @@
+package com.apexpathing.util;
+
+/**
+ * A squID controller for follower
+ * @author Xander Haemel - 31616
+ */
+public class SquidController {
+    private double kP, kI, kD;
+    private double goal;
+}
